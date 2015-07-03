995b5f0df064b6f43f62e34ba3633daed018bebc
i-am-gabi <gabicavalcantesilva@gmail.com>
2015-07-03 17:55:14 -0300

    adicionando modal

--- dimap/30anos/js/modal.js
+++ dimap/30anos/js/modal.js
@@ -1,20 +1,8 @@
-              $("#myModal").on("show", function() {    // wire up the OK button to dismiss the modal when shown
-                  $("#myModal a.btn").on("click", function(e) {
-                      console.log("button pressed");   // just as an example...
-                      $("#myModal").modal('hide');     // dismiss the dialog
-                  });
-              });
+function overlay() {
+	el = document.getElementById("overlay");
+	el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
+}
 
-              $("#myModal").on("hide", function() {    // remove the event listeners when the dialog is dismissed
-                  $("#myModal a.btn").off("click");
-              });
-              
-              $("#myModal").on("hidden", function() {  // remove the actual elements from the DOM when fully hidden
-                  $("#myModal").remove();
-              });
-              
-              $("#myModal").modal({                    // wire up the actual modal functionality and show the dialog
-                "backdrop"  : "static",
-                "keyboard"  : true,
-                "show"      : true                     // ensure the modal is shown immediately
-              }); 
+function close() {
+    document.getElementById("overlay").style.visibility = 'hidden';
+}
